ab6790a7e76b9927b7eedb13f816a0830cb88607
EbukaChuqz <ebukachuqz@gmail.com>
2022-06-21 18:41:44 +0100

    Connected to MogoDb and saved scrapped data in DB

--- api/app.js
+++ api/app.js
@@ -1,13 +1,20 @@
+require("dotenv").config();
 const express = require("express");
+const connectDB = require("./db/connectDB");
 const app = express();
+const cors = require("cors");
+const { scrapeData } = require("./utils/scrapper");
 
 // MiddleWares
 app.use(express.json);
-app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 const port = process.env.PORT || 4000;
-const startApp = () => {
+const startApp = async () => {
+  await connectDB(process.env.MONGO_URI);
   app.listen(port, () => console.log(`app is listening on ${port}`));
+  scrapeData(); // initial call to scrape data as server fires up
+  setInterval(scrapeData, 60000); // scrape data every 5mins
 };
 
 startApp();
---
+++ api/model/ScrappedData.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const ScrappedDataSchema = new mongoose.Schema({
+  balance: {
+    type: String,
+    required: true,
+  },
+
+  equity: {
+    type: String,
+    required: true,
+  },
+
+  marketwatchTime: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("ScrappedData", ScrappedDataSchema);
