721b9ac851940b5cc5dc9922794d42573213c586
EbukaChuqz <ebukachuqz@gmail.com>
2022-06-24 00:22:41 +0100

    Added timestamps to schema

--- api/model/ScrappedData.js
+++ api/model/ScrappedData.js
@@ -1,25 +1,28 @@
 const mongoose = require("mongoose");
 
-const ScrappedDataSchema = new mongoose.Schema({
-  balance: {
-    type: String,
-    required: true,
-  },
+const ScrappedDataSchema = new mongoose.Schema(
+  {
+    balance: {
+      type: String,
+      required: true,
+    },
 
-  equity: {
-    type: String,
-    required: true,
-  },
+    equity: {
+      type: String,
+      required: true,
+    },
 
-  currency: {
-    type: String,
-    required: true,
-  },
+    currency: {
+      type: String,
+      required: true,
+    },
 
-  marketwatchTime: {
-    type: String,
-    required: true,
+    marketwatchTime: {
+      type: String,
+      required: true,
+    },
   },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("ScrappedData", ScrappedDataSchema);
